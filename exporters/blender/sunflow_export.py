e4ffe105284233efb22eb84d6e8def983913cfe2
Eugene Reilly <gene001@gmail.com>
2007-05-22 12:59:22 +0000

    Added GPL license block since there has been a push to add it to all scripts bundled with Blender.  This script isn't packaged with Blender, but this should make it compliant if it ever does get added.

--- exporters/blender/sunflow_export.py
+++ exporters/blender/sunflow_export.py
@@ -4,15 +4,31 @@
 Name: 'Sunflow Exporter 1.3.1 (.sc)...'
 Blender: 2.44
 Group: 'Export'
-Tip: ''
-"""
+Tip: 'Export to a Sunflow Scene File'
 
-"""
 Version         :       1.3.1 (May 2007)
 Author          :       R Lindsay (hayfever) / Christopher Kulla / MADCello / 
 			olivS / Eugene Reilly / Heavily Tessellated / Humfred
 Description     :       Export to Sunflow renderer http://sunflow.sourceforge.net/
 Usage           :       See how to use the script at http://sunflow.sourceforge.net/phpbb2/viewtopic.php?t=125
+
+***** BEGIN GPL LICENSE BLOCK *****
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+***** END GPL LICENCE BLOCK *****
 """
 
 ####### General Script Properties Section #######
